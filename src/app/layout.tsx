6a9bff6d75c54806b2ead0e315841c3ad85834ee
Diego Amundaray <112201548+Aegox@users.noreply.github.com>
2025-01-22 21:20:11 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Ubuntu_Sans } from "next/font/google";
+import { Ubuntu_Sans } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const ubuntuSans = Ubuntu_Sans({
   weight: ["500", "600"],
